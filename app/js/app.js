37a179ae5dd498036f3fecd28bfce6c9e9974672
Spencer Janssen <spencer@well-typed.com>
2014-07-18 14:04:51 -0500

    Add additional scribblings

--- app/js/app.js
+++ app/js/app.js
@@ -12,5 +12,6 @@ angular.module('myApp', [
 config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {templateUrl: 'partials/partial1.html', controller: 'MyCtrl1'});
   $routeProvider.when('/view2', {templateUrl: 'partials/partial2.html', controller: 'MyCtrl2'});
+  $routeProvider.when('/sets', {templateUrl: 'partials/sets.html', controller: 'SetsController'});
   $routeProvider.otherwise({redirectTo: '/view1'});
 }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,9 +3,16 @@
 /* Controllers */
 
 angular.module('myApp.controllers', [])
-  .controller('MyCtrl1', ['$scope', function($scope) {
+    .controller('MyCtrl1', ['$scope', function($scope) {
 
-  }])
-  .controller('MyCtrl2', ['$scope', function($scope) {
+    }])
+    .controller('MyCtrl2', ['$scope', function($scope) {
 
-  }]);
+    }])
+    .controller('SetsController', function($scope, $http) {
+        console.log('sets controller loaded');
+        $http.get('api/sets.json').success(function(data){
+            $scope.sets = data
+        });
+  })
+  ;
