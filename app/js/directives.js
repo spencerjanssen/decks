09f0f10460cbad8245ba600405c048b6278642ea
spencerjanssen <SpencerJanssen@gmail.com>
2014-08-13 16:31:40 -0500

    Add card-symbols directive
    Display card symbols in a nicer fashion.
    We now depend on a lexer package.

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,8 +1,6 @@
 'use strict';
 
 /* Directives */
-
-
 angular.module('myApp.directives', [])
     // href to a cards details page. If inner html is empty, plug the card name
     // in as well
@@ -16,4 +14,19 @@ angular.module('myApp.directives', [])
                        element.html(scope.card.name);
                    }
                }}
-    });
+    })
+    .directive('cardSymbols', function() {
+        return { restrict: 'A'
+               , link: function(scope, element, attrs){
+                    var output = '';
+                    var lex = (new Lexer())
+                        .setInput(attrs.cardSymbols)
+                        .addRule(/{([^}]+)}/, function(l, sym) {
+                            sym = sym.replace('/', '');
+                            output += '<img src="http://mtgimage.com/symbol/mana/' + sym + '/16.png"></img>';
+                            return sym ;
+                        });
+                    while(lex.lex()) {};
+                    element.html(output);
+               }};
+    });;
