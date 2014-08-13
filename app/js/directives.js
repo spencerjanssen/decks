20f9539b2e7e3b79cd4f64d86e7b27372a63d4b9
spencerjanssen <SpencerJanssen@gmail.com>
2014-08-13 11:43:31 -0500

    Add spoiler view, card-link directive

--- app/js/directives.js
+++ app/js/directives.js
@@ -3,4 +3,17 @@
 /* Directives */
 
 
-angular.module('myApp.directives', []);
+angular.module('myApp.directives', [])
+    // href to a cards details page. If inner html is empty, plug the card name
+    // in as well
+    // <a card-link="scope to retrive card name">INNER HTML</a>
+    .directive('cardLink', function() {
+        return { restrict: 'A'
+               , scope: {card: '=cardLink'}
+               , link: function(scope, element, attrs){
+                   element.attr('href', '#/card/' + scope.card.name);
+                   if(element.html() == ''){
+                       element.html(scope.card.name);
+                   }
+               }}
+    });
