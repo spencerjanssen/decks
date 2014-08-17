5082a817cadc6d3ad90f769a0acdb64be0cf69ba
Spencer Janssen <spencer@well-typed.com>
2014-08-17 16:40:46 -0500

    querycompiler: use Unix backslash

--- backend/querycompiler.js
+++ backend/querycompiler.js
@@ -5,7 +5,7 @@ o:<card text>
 
 var fs = require("fs");
 var jison= require("jison");
-var bnf = fs.readFileSync("backend\\query.jison", "utf8");
+var bnf = fs.readFileSync("backend/query.jison", "utf8");
 var parser = new jison.Parser(bnf);
 
 /*
