fbbb5807615242ef68b5dca6ed526952fe006c2b
spencerjanssen <SpencerJanssen@gmail.com>
2014-08-18 17:37:28 -0500

    Disable bad tests, update controller tests

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -7,6 +7,8 @@ module.exports = function(config){
       'app/bower_components/angular/angular.js',
       'app/bower_components/angular-route/angular-route.js',
       'app/bower_components/angular-mocks/angular-mocks.js',
+      'app/bower_components/angular-resource/angular-resource.js',
+      'app/bower_components/lexer/lexer.js',
       'app/js/**/*.js',
       'test/unit/**/*.js'
     ],
--- test/unit/controllersSpec.js
+++ test/unit/controllersSpec.js
@@ -4,17 +4,41 @@
 
 describe('controllers', function(){
   beforeEach(module('myApp.controllers'));
+  beforeEach(module('myApp.services'));
+  beforeEach(module('ngRoute'));
 
+  // add page.title to root scope, since some controllers expect it
+  var titlescope = { page: {setTitle: function(title) {
+      this.title = title;
+  }}};
 
   it('should ....', inject(function($controller) {
-    //spec body
-    var myCtrl1 = $controller('MyCtrl1', { $scope: {} });
-    expect(myCtrl1).toBeDefined();
+    var ctl = $controller('SetController', { $scope: titlescope });
+    expect(ctl).toBeDefined();
   }));
 
   it('should ....', inject(function($controller) {
-    //spec body
-    var myCtrl2 = $controller('MyCtrl2', { $scope: {} });
-    expect(myCtrl2).toBeDefined();
+    var ctl = $controller('SetsController', { $scope: titlescope });
+    expect(ctl).toBeDefined();
+  }));
+
+  it('should ....', inject(function($controller) {
+    var ctl = $controller('CardController', { $scope: titlescope });
+    expect(ctl).toBeDefined();
+  }));
+
+  it('should ....', inject(function($controller) {
+    var ctl = $controller('AdvSearchController', { $scope: titlescope });
+    expect(ctl).toBeDefined();
+  }));
+
+  it('should ....', inject(function($controller) {
+    var ctl = $controller('QuerySearchController', { $scope: titlescope });
+    expect(ctl).toBeDefined();
+  }));
+
+  it('should ....', inject(function($controller) {
+    var ctl = $controller('HeaderController', { $scope: titlescope });
+    expect(ctl).toBeDefined();
   }));
 });
