b2b187aa96120f9430127772e6a1ef73616f8cfa
spencerjanssen <SpencerJanssen@gmail.com>
2014-08-19 14:19:40 -0500

    controllers.js: more consistent indentation

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -5,27 +5,27 @@
 angular.module('myApp.controllers', [])
     .controller('SetsController', function($scope, $http, Sets) {
         $scope.sets = Sets.getsetlist.query();
-  })
+    })
     .controller('SetController', function($scope, $routeParams, $http, Sets) {
         $scope.cards = Sets.getset.query({setcode: $routeParams.setcode});
         // TODO actually look up set info and display nice name
         $scope.set = {setname: $routeParams.setcode};
         $scope.page.setTitle('Set: ' + $routeParams.setcode);
-  })
+    })
     .controller('CardController', function($scope, $routeParams, $http, Sets) {
         $scope.card = Sets.getcard.get({card: $routeParams.cardname});
         $scope.page.setTitle('Card: ' + $routeParams.cardname);
-  })
+    })
     .controller('AdvSearchController', function($scope, Sets) {
         $scope.search = function(q){
             $scope.cards = Sets.searchcards.update({}, q);
         }
-  })
+    })
     .controller('QuerySearchController', function($scope, Sets) {
         $scope.search = function(q){
             $scope.cards = Sets.querysearch.query({query: q});
         }
-  })
+    })
     .controller('HeaderController', function($scope, $location){
         $scope.active = function(loc){
             if(loc == $location.path()){
@@ -35,4 +35,4 @@ angular.module('myApp.controllers', [])
             }
         }
     })
-  ;
+;
